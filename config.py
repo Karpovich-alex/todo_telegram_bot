9185b0f36376a2fab62283179b79abdfc62a6c08
Karpovich_alex_mpc <karpovehalex@gmail.com>
2021-01-10 13:04:26 +0300

    Add req and fix config

--- config.py
+++ config.py
@@ -7,6 +7,4 @@ load_dotenv(os.path.join(basedir, 'vars.env'))
 
 class Config:
     bot_api = os.environ.get('BOT_API')
-    # SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///:memory:?check_same_thread=False'
-    # ?check_same_thread=False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/test'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_PATH') or 'sqlite:///:memory:'
